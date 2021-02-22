6dd183731f502a1a82ac94f896e0e967ac785af7
gserban <gserban1233@gmail.com>
2021-02-22 19:11:58 +0200

    fullmatch

--- password checker.py
+++ password checker.py
@@ -9,7 +9,7 @@ password = []
 
 def validator_function():
     string = input('Enter a password. It can contain letters, numbers and/or $ % # @ and must be more then 8 characters long: ')
-    validation = pattern.search(string)
+    validation = pattern.fullmatch(string)
     if validation == None:
         print('-> Please use letters, numbers or $ % # @ and it has to be more then 8 characters long!')
         validator_function()
